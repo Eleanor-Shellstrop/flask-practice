b0dbf9b3e27396d8b4e6848b7e888fadc725a234
Anne Ensign <aensign@virtual-peaker.com>
2022-06-08 16:39:56 -0400

    Test nodes in linked lists

--- linked_list.py
+++ linked_list.py
@@ -10,9 +10,24 @@ class LinkedList:
         self.head = None
         self.last_node = None
 
-#           Node2                             Node1
-# _______________________            ________________________
-# |                     |            |                      |
-# |  Data    |  Node1   |--------->  |  Data    |   *       |----> None
-# |                     |            |                      |
-# _______________________            ________________________
+    def print_ll(self):
+        ll_string = ""
+        node = self.head
+        if node is None:
+            print(None)
+        while node:
+            ll_string += f" {str(node.data)} ->"
+            node = node.next_node
+        ll_string += "None"
+        print(ll_string)
+
+
+ll = LinkedList()
+node4 = Node("data4", None)
+node3 = Node("data3", node4)
+node2 = Node("data2", node3)
+node1 = Node("data1", node2)
+
+ll.head = node1
+
+ll.print_ll()
